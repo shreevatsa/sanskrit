977e4a0b316d282e42370e45a27605ac3049a427
Shreevatsa R <shreevatsa.public@gmail.com>
2013-10-21 23:57:55 +0530

    First version of files to work with AppEngine

---
+++ request_handler.py
@@ -0,0 +1,38 @@
+"""The class that actually handles the input verse."""
+
+import cgi
+import webapp2
+
+import sscan
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/identify" method="post">
+      <div><textarea name="input_verse" rows="6" cols="80"></textarea></div>
+      <div><input type="submit" value="Identify verse"></div>
+    </form>
+  </body>
+</html>
+"""
+
+
+class InputPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.write(MAIN_PAGE_HTML)
+
+class IdentifyPage(webapp2.RequestHandler):
+  def post(self):
+    self.response.write('<html><body>You wrote:<pre>')
+    self.response.write(cgi.escape(self.request.get('input_verse')))
+    # TODO(shreevatsa): Ridiculous that this runs each time; needs fixing (easy).
+    sscan.InitializeData()
+    metre = sscan.IdentifyFromLines(input_verse.split('\n'))
+    self.response.write(metre)
+    self.response.write('</pre></body></html>')
+
+
+application = webapp2.WSGIApplication([
+  ('/', InputPage),
+  ('/identify', IdentifyPage),
+], debug=True)
