4459e2a6f2af9bdf1b30ea28fae2227284263b4b
Shreevatsa R <shreevatsa.public@gmail.com>
2013-10-21 07:50:40 +0530

    move variables inside

--- sscan.py
+++ sscan.py
@@ -7,14 +7,6 @@ import re
 import string
 import sys
 
-# A regular-expression "character class" for each type
-consonant = '[MHkgGcjJTDNtdnpbmyrlvzSsh]'
-alpaprana = '[kgcjTDtdpb]'
-short_vowel = '[aiuR]'
-long_vowel = '[AIUeo]'
-vowel = short_vowel.strip(']') + long_vowel.strip('[')
-valid_hk = 'aAiIuUReoMHkgGcjJTDNtdnpbmyrlvzSsh'
-
 
 def MassageHK(text):
   """Rewrite keeping metre intact (multiple characters -> single char)."""
@@ -24,12 +16,14 @@ def MassageHK(text):
   text = re.sub('RR', 'e', text)
   text = re.sub('a[iu]', 'e', text)
   # Two-letter consonants: replace 'kh' by 'k', etc.
+  alpaprana = '[kgcjTDtdpb]'
   text = re.sub('(' + alpaprana + ')' + 'h', r'\g<1>', text)
   return text
 
 
 def CheckHK(text):
   """Check that a block of text contains only HK characters."""
+  valid_hk = 'aAiIuUReoMHkgGcjJTDNtdnpbmyrlvzSsh'
   bad_match = re.search('[^%s]' % valid_hk, text)
   if bad_match:
     first_index = bad_match.start()
@@ -43,6 +37,11 @@ def CheckHK(text):
 def MetricalPattern(text):
   """Given text in HK, print its metrical pattern (string of 'L's and 'G's)."""
   assert CheckHK(text)
+  # A regular-expression "character class" for each type
+  consonant = '[MHkgGcjJTDNtdnpbmyrlvzSsh]'
+  short_vowel = '[aiuR]'
+  long_vowel = '[AIUeo]'
+  vowel = short_vowel.strip(']') + long_vowel.strip('[')
   # Consonants at beginning of text can be ignored
   text = re.sub('^' + consonant + '+', '', text)
   # A long vowel followed by any number of consonants is a guru
@@ -67,7 +66,7 @@ def IdentitfyPattern(pattern):
   """Given metrical pattern (string of L's and G's), identify metre."""
   if not re.match('^[LG]*$', pattern):
     print '%s is not a pattern (must have only L and G)' % pattern
-  return known_patterns.get(pattern)
+  return known_patterns.get(pattern, 'unknown')
 
 
 def IdentifyMetre(verse):
@@ -75,18 +74,15 @@ def IdentifyMetre(verse):
   full_verse = ''.join(verse)
   print 'The input has %d syllables' % len(full_verse)
 
-  print 'Trying as a whole: '
   for known_pattern, known_metre in known_metres.iteritems():
     if re.match('^' + known_pattern + '$', full_verse):
-      print '  Identified as %s' % known_metre
+      print 'Identified as %s' % known_metre
       return known_metre
-  print '  No metre found.'
 
-  if len(verse) == 4:
-    print 'Trying by lines: '
-    for i in range(4):
-      line = verse[i]
-      print '  Line %d: pattern %s is %s' % (i, line, IdentitfyPattern(line))
+  print 'Metre unknown, trying by lines: '
+  for i in range(len(verse)):
+    line = verse[i]
+    print '  Line %d: pattern %s is %s' % (i, line, IdentitfyPattern(line))
 
 
 if __name__ == '__main__':
@@ -103,6 +99,6 @@ if __name__ == '__main__':
 
     pattern_lines.append(MetricalPattern(line))
 
-  print IdentifyMetre(pattern_lines)
+  identified_metre = IdentifyMetre(pattern_lines)
 
 
