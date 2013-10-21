3863d5a9f0da34800739904d7b6d9f140ad48a22
Shreevatsa R <shreevatsa.public@gmail.com>
2013-10-22 00:52:29 +0530

    capture stdout and restore it

--- request_handler.py
+++ request_handler.py
@@ -2,6 +2,7 @@
 
 import cgi
 import StringIO
+import sys
 
 import webapp2
 
@@ -26,13 +27,21 @@ class InputPage(webapp2.RequestHandler):
 
 class IdentifyPage(webapp2.RequestHandler):
   def post(self):
+    """What to do with the posted input string (verse)."""
     self.response.write('<html><body>You wrote:<pre>')
     input_verse = self.request.get('input_verse')
     self.response.write(cgi.escape(input_verse))
+    self.response.write('\n\n\n')
+    # TODO(shreevatsa): Get rid of this hack for capturing stdout.
+    stdout_original = sys.stdout
+    stdout_new = StringIO.StringIO()
+    sys.stdout = stdout_new
     # TODO(shreevatsa): Ridiculous that this runs each time; needs fixing (easy)
     sscan.InitializeData()
-    metre = sscan.IdentifyFromLines(input_verse.split('\n'))
-    self.response.write(metre)
+    sscan.IdentifyFromLines(input_verse.split('\n'))
+    output = stdout_new.getvalue()
+    self.response.write(output)
+    sys.stdout = stdout_original
     self.response.write('</pre></body></html>')
 
 
