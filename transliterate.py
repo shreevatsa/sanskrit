a1a529ccd6c81027e55fae75f39a83f69c729df5
Shreevatsa R <shreevatsa.public@gmail.com>
2013-11-06 00:00:40 +0530

    move out

--- transliterate.py
+++ transliterate.py
@@ -34,6 +34,7 @@ seeing that character, which state to go to, and how many characters to consume.
 from __future__ import unicode_literals
 
 import devanagari
+import transliteration_data
 
 
 def MakeStateMachine(table):
@@ -83,67 +84,15 @@ def Transliterate(state_machine, text, pass_through=None):
   return (transliterated, unparsed_positions)
 
 
-def AlphabetToSLP1(alphabet):
-  """Table, given a transliteration convention's alphabet in standard order."""
-  return dict(zip(alphabet,
-                  'aAiIuUfFxXeEoOMHkKgGNcCjJYwWqQRtTdDnpPbBmyrlvSzsh'))
-
-
-def HKToSLP1Table():
-  return AlphabetToSLP1(list('aAiIuUR') +
-                        ['RR', 'lR', 'lRR', 'e', 'ai', 'o', 'au', 'M', 'H',
-                         'k', 'kh', 'g', 'gh', 'G',
-                         'c', 'ch', 'j', 'jh', 'J',
-                         'T', 'Th', 'D', 'Dh', 'N',
-                         't', 'th', 'd', 'dh', 'n',
-                         'p', 'ph', 'b', 'bh', 'm'] +
-                        list('yrlvzSsh'))
-
-
-def IASTToSLP1Table():
-  """Transliteration table from IAST to SLP1."""
-  lower = AlphabetToSLP1(list('aāiīuūṛṝḷḹe') + ['ai', 'o', 'au', 'ṃ', 'ḥ'] +
-                         ['k', 'kh', 'g', 'gh', 'ṅ',
-                          'c', 'ch', 'j', 'jh', 'ñ',
-                          'ṭ', 'ṭh', 'ḍ', 'ḍh', 'ṇ',
-                          't', 'th', 'd', 'dh', 'n',
-                          'p', 'ph', 'b', 'bh', 'm',
-                          'y', 'r', 'l', 'v', 'ś', 'ṣ', 's', 'h'])
-  upper = AlphabetToSLP1(list('AĀIĪUŪṚṜḶḸE') + ['AI', 'O', 'AU', 'Ṃ', 'Ḥ'] +
-                         ['K', 'Kh', 'G', 'Gh', 'Ṅ',
-                          'C', 'Ch', 'J', 'Jh', 'Ñ',
-                          'Ṭ', 'Ṭh', 'Ḍ', 'Ḍh', 'Ṇ',
-                          'T', 'Th', 'D', 'Dh', 'N',
-                          'P', 'Ph', 'B', 'Bh', 'M',
-                          'Y', 'R', 'L', 'V', 'Ś', 'Ṣ', 'S', 'H'])
-  lower.update(upper)
-  upper.update(lower)
-  assert lower == upper
-  return lower
-
-
-def ITRANSToSLP1Table():
-  return AlphabetToSLP1(['a', 'aa', 'i', 'ii', 'u', 'uu', 'Ri', 'RI',
-                         'Li', 'LI', 'e', 'ai', 'o', 'au', 'M', 'H',
-                         'k', 'kh', 'g', 'gh', '~N',
-                         'ch', 'Ch', 'j', 'jh', '~n',
-                         'T', 'Th', 'D', 'Dh', 'N',
-                         't', 'th', 'd', 'dh', 'n',
-                         'p', 'ph', 'b', 'bh', 'm',
-                         'y', 'r', 'l', 'v', 'sh', 'Sh', 's', 'h'])
-
-
-def MangledDevanagariToSLP1Table():
-  return AlphabetToSLP1(devanagari.Alphabet())
-
-
 def TransliterateDevanagari(text):
-  return Transliterate(MakeStateMachine(MangledDevanagariToSLP1Table()),
+  return Transliterate(MakeStateMachine(
+      transliteration_data.MangledDevanagariToSLP1Table()),
                        devanagari.Mangle(text))
 
 
 def TransliterateHK(text, pass_through=None):
-  return Transliterate(MakeStateMachine(HKToSLP1Table()), text, pass_through)
+  return Transliterate(MakeStateMachine(transliteration_data.HKToSLP1Table()),
+                       text, pass_through)
 
 
 blah = devanagari.Mangle('कगुद')
---
+++ transliteration_data.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""Transliteration data."""
+
+from __future__ import unicode_literals
+
+import devanagari
+
+
+def AlphabetToSLP1(alphabet):
+  """Table, given a transliteration convention's alphabet in standard order."""
+  return dict(zip(alphabet,
+                  'aAiIuUfFxXeEoOMHkKgGNcCjJYwWqQRtTdDnpPbBmyrlvSzsh'))
+
+
+def HKToSLP1Table():
+  return AlphabetToSLP1(list('aAiIuUR') +
+                        ['RR', 'lR', 'lRR', 'e', 'ai', 'o', 'au', 'M', 'H',
+                         'k', 'kh', 'g', 'gh', 'G',
+                         'c', 'ch', 'j', 'jh', 'J',
+                         'T', 'Th', 'D', 'Dh', 'N',
+                         't', 'th', 'd', 'dh', 'n',
+                         'p', 'ph', 'b', 'bh', 'm'] +
+                        list('yrlvzSsh'))
+
+
+def IASTToSLP1Table():
+  """Transliteration table from IAST to SLP1."""
+  lower = AlphabetToSLP1(list('aāiīuūṛṝḷḹe') + ['ai', 'o', 'au', 'ṃ', 'ḥ'] +
+                         ['k', 'kh', 'g', 'gh', 'ṅ',
+                          'c', 'ch', 'j', 'jh', 'ñ',
+                          'ṭ', 'ṭh', 'ḍ', 'ḍh', 'ṇ',
+                          't', 'th', 'd', 'dh', 'n',
+                          'p', 'ph', 'b', 'bh', 'm',
+                          'y', 'r', 'l', 'v', 'ś', 'ṣ', 's', 'h'])
+  upper = AlphabetToSLP1(list('AĀIĪUŪṚṜḶḸE') + ['AI', 'O', 'AU', 'Ṃ', 'Ḥ'] +
+                         ['K', 'Kh', 'G', 'Gh', 'Ṅ',
+                          'C', 'Ch', 'J', 'Jh', 'Ñ',
+                          'Ṭ', 'Ṭh', 'Ḍ', 'Ḍh', 'Ṇ',
+                          'T', 'Th', 'D', 'Dh', 'N',
+                          'P', 'Ph', 'B', 'Bh', 'M',
+                          'Y', 'R', 'L', 'V', 'Ś', 'Ṣ', 'S', 'H'])
+  lower.update(upper)
+  upper.update(lower)
+  assert lower == upper
+  return lower
+
+
+def ITRANSToSLP1Table():
+  return AlphabetToSLP1(['a', 'aa', 'i', 'ii', 'u', 'uu', 'Ri', 'RI',
+                         'Li', 'LI', 'e', 'ai', 'o', 'au', 'M', 'H',
+                         'k', 'kh', 'g', 'gh', '~N',
+                         'ch', 'Ch', 'j', 'jh', '~n',
+                         'T', 'Th', 'D', 'Dh', 'N',
+                         't', 'th', 'd', 'dh', 'n',
+                         'p', 'ph', 'b', 'bh', 'm',
+                         'y', 'r', 'l', 'v', 'sh', 'Sh', 's', 'h'])
+
+
+def MangledDevanagariToSLP1Table():
+  return AlphabetToSLP1(devanagari.Alphabet())
+
+
