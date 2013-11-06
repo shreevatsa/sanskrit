72fbefdce993b82eb80f8a6742ee1982d7d1d8e5
Shreevatsa R <shreevatsa.public@gmail.com>
2013-11-06 23:47:45 +0530

    ignore more

--- read_gretil.py
+++ read_gretil.py
@@ -11,7 +11,7 @@ def RemoveHTML(text):
 
 
 def RemoveVerseNumber(text):
-  return re.sub('//[ \d.a}]*//$', '', text)
+  return re.subn('[/|]{2}[ \d.a-zA-z}_*]*[/|]{2}$', '', text)
 
 
 if __name__ == '__main__':
@@ -24,11 +24,14 @@ if __name__ == '__main__':
     if seen_separators < 2 and re.search('<![-]{50,}->', l):
       seen_separators += 1
     elif seen_separators == 2 and not l.startswith('<'):
+      (l, n) = RemoveVerseNumber(l)
       lines.append(l)
+      if n:
+        lines.append('')
 
   verses = handle_input.BreakIntoVerses(lines)
   for verse in verses:
     print '\n\t\t\t\t\t\t'.join([''] + verse).encode('utf8')
-    verse = [RemoveVerseNumber(l).strip() for l in verse]
+    verse = [l.strip() for l in verse]
     sscan.IdentifyFromLines(verse)
     print
