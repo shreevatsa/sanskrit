548119dacfd2505f1140fa4128b2aac4729e85a7
Shreevatsa R <shreevatsa.public@gmail.com>
2013-11-14 03:16:47 +0530

    but only if necessary

--- request_handler.py
+++ request_handler.py
@@ -70,7 +70,8 @@ class IdentifyPage(webapp2.RequestHandler):
 
     self.response.write('<p><i>Debugging output:</i></p>')
     self.response.write('<pre>')
-    self.response.write('\n'.join(identifier.cleaned_output))
+    if metre:                             # else the debug output already has it
+      self.response.write('\n'.join(identifier.cleaned_output))
     self.response.write(identifier.AllDebugOutput())
     self.response.write('</pre></body></html>')
 
