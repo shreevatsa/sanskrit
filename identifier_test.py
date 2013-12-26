c5ac0bde24c19240c244cf4939b5fd7092cd3a68
Shreevatsa R <shreevatsa.public@gmail.com>
2013-12-26 20:50:02 +0530

    couple more tests

--- identifier_test.py
+++ identifier_test.py
@@ -42,7 +42,7 @@ class KnownValues(unittest.TestCase):
       result = results[0]
       assert result.metre_name == metre_name
       assert result.match_type == match_type
-      assert result.issues == issues
+      assert result.issues == set(issues)
     except AssertionError:
       print('Mismatch: Got results')
       print(match_result.Description(results))
@@ -52,14 +52,59 @@ class KnownValues(unittest.TestCase):
       print('Issues: %s' % issues)
       raise self.failureException
 
-  def testFineAnustup(self):
-    """Good anuṣṭup must be recognized."""
-    verse = ['agajānana padmārkaṃ gajānanam aharniṣam anekadantam',
-             'bhaktānām ekadantam upāsmahe']
+  def testFineAnustubh(self):
+    """Good anuṣṭubh should be recognized."""
+    verse = ['agajānana padmārkaṃ',
+             'gajānanam aharniṣam',
+             'anekadantam bhaktānām',
+             'ekadantam upāsmahe']
     self.AssertSingleMatchResultEquals(identifier.IdentifyFromLines(verse),
                                        'Anuṣṭup (Śloka)',
                                        match_result.MATCH_TYPE.FULL,
                                        [])
+
+  def testFirstOffAnustubh(self):
+    """Anuṣṭubh with first pāda wrong should be recognized."""
+    verse = ['तपःस्वाध्यायनिरतं तपस्वी वाग्विदां वरम् ।',
+             'नारदं परिपप्रच्छ वाल्मीकिर्मुनिपुङ्गवम् ।। 1.1.1 ।।']
+    self.AssertSingleMatchResultEquals(
+        identifier.IdentifyFromLines(verse),
+        'Anuṣṭup (Śloka)',
+        match_result.MATCH_TYPE.FULL,
+        [match_result.ISSUES.FIRST_PADA_OFF,
+         match_result.ISSUES.VISAMA_PADANTA_LAGHU])
+
+  def testFineMandakranta(self):
+    """A valid example of Mandākrāntā should be recognized."""
+    verse = ['kazcit kAntAvirahaguruNA svAdhikArAt pramattaH $',
+             'zApenAstaMgamitamahimA varSabhogyeNa bhartuH &',
+             'yakSaz cakre janakatanayAsnAnapuNyodakeSu %',
+             'snigdhacchAyAtaruSu vasatiM rAmagiryAzrameSu // 1.1 //']
+    self.AssertSingleMatchResultEquals(
+        identifier.IdentifyFromLines(verse),
+        'Mandākrāntā',
+        match_result.MATCH_TYPE.FULL,
+        [])
+
+  def testMandakrantaWithVpl(self):
+    """Mandākrāntā with viṣama-pādānta-laghu."""
+    verse = ['tvayy AdAtuM jalam avanate zArGgiNo varNacaure $',
+             'tasyAH sindhoH pRthum api tanuM dUrabhAvAt pravAham &',
+             'prekSiSyante gaganagatayo nUnam Avarjya dRSTir %',
+             'ekaM bhuktAguNam iva bhuvaH sthUlamadhyendranIlam // 1.49 //']
+    self.AssertSingleMatchResultEquals(
+        identifier.IdentifyFromLines(verse),
+        'Mandākrāntā',
+        match_result.MATCH_TYPE.FULL,
+        [match_result.ISSUES.VISAMA_PADANTA_LAGHU])
+
+  def testArya(self):
+    """Test some examples of Arya."""
+    pass
+
+  def testBreakUp(self):
+    """Test all the above by inserting random breaks in the list."""
+    pass
 
 
 if __name__ == '__main__':
