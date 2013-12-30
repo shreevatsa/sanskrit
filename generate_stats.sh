b3ce9f84624559cf7645f72a0894ee2556b778c1
Shreevatsa R <shreevatsa.public@gmail.com>
2013-12-31 00:00:28 +0530

    remove section headings in Nilakantha, for better stats

--- generate_stats.sh
+++ generate_stats.sh
@@ -7,6 +7,6 @@ patch ~/GRETIL/forme/amaru_u.htm -o /tmp/amaru_u.htm < diff-amaru_u.htm.patch &&
 ./read_gretil.py ~/GRETIL/forme/maghspvu.htm --print_identified_verses=none
 patch ~/GRETIL/forme/meghdk_u.htm -o /tmp/meghdk_u.htm < diff-meghdk_u.htm.patch && ./read_gretil.py /tmp/meghdk_u.htm --print_identified_verses=none
 ./read_gretil.py ~/GRETIL/forme/msubhs_u.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/nkalivpu.htm --print_identified_verses=none
+patch ~/GRETIL/forme/nkalivpu.htm -o /tmp/nkalivpu.htm < diff-nkalivpu.htm.patch && ./read_gretil.py /tmp/nkalivpu.htm --print_identified_verses=none
 patch ~/GRETIL/forme/ramodtpu.htm -o /tmp/ramodtpu.htm < diff-ramodtpu.htm.patch && ./read_gretil.py /tmp/ramodtpu.htm --print_identified_verses=none
 python generate_stats_table.py > stats_table.html
