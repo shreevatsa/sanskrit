3e9433b34a7660c9db4df593a3d32d6c24bda8e3
Shreevatsa R <shreevatsa.public@gmail.com>
2013-12-31 00:48:28 +0530

    simplify generate_stats.sh

--- generate_stats.sh
+++ generate_stats.sh
@@ -1,12 +1,22 @@
-patch ~/GRETIL/forme/amaru_u.htm -o /tmp/amaru_u.htm < diff-amaru_u.htm.patch && ./read_gretil.py /tmp/amaru_u.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/bhakirpu.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/bhall_pu.htm --print_identified_verses=none
+function scan() {
+  if [ -f diff-$1.patch ];
+    then
+      patch ~/GRETIL/forme/$1 -o /tmp/$1 < diff-$1.patch && ./read_gretil.py /tmp/$1 --print_identified_verses=none
+  else
+      ./read_gretil.py ~/GRETIL/forme/$1 --print_identified_verses=none
+  fi
+}
+
+scan amaru_u.htm
+scan bhakirpu.htm
+scan bhall_pu.htm
 ./read_gretil.py bharst_u.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/kakumspu.htm --print_identified_verses=none
+scan kakumspu.htm
 ./read_gretil.py kragh_pu.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/maghspvu.htm --print_identified_verses=none
-patch ~/GRETIL/forme/meghdk_u.htm -o /tmp/meghdk_u.htm < diff-meghdk_u.htm.patch && ./read_gretil.py /tmp/meghdk_u.htm --print_identified_verses=none
-./read_gretil.py ~/GRETIL/forme/msubhs_u.htm --print_identified_verses=none
-patch ~/GRETIL/forme/nkalivpu.htm -o /tmp/nkalivpu.htm < diff-nkalivpu.htm.patch && ./read_gretil.py /tmp/nkalivpu.htm --print_identified_verses=none
-patch ~/GRETIL/forme/ramodtpu.htm -o /tmp/ramodtpu.htm < diff-ramodtpu.htm.patch && ./read_gretil.py /tmp/ramodtpu.htm --print_identified_verses=none
+scan maghspvu.htm
+scan meghdk_u.htm
+scan msubhs_u.htm
+scan nkalivpu.htm
+scan ramodtpu.htm
+
 python generate_stats_table.py > stats_table.html
