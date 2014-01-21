215db928dbb04c2df4720c5348f9b98a96a4e38c
Shreevatsa R <shreevatsa.public@gmail.com>
2014-01-21 07:26:10 +0530

    need ../read_gretil.py now

--- gretil_stats/generate_stats.sh
+++ gretil_stats/generate_stats.sh
@@ -1,22 +1,22 @@
 function scan() {
   if [ -f diff-$1.patch ];
     then
-      patch ~/GRETIL/forme/$1 -o /tmp/$1 < diff-$1.patch && ./read_gretil.py /tmp/$1 --print_identified_verses=none --print_unidentified_verses=none
+      patch ~/GRETIL/forme/$1 -o /tmp/$1 < diff-$1.patch && ../read_gretil.py /tmp/$1 --print_identified_verses=none --print_unidentified_verses=none
   else
-      ./read_gretil.py ~/GRETIL/forme/$1 --print_identified_verses=none --print_unidentified_verses=none
+      ../read_gretil.py ~/GRETIL/forme/$1 --print_identified_verses=none --print_unidentified_verses=none
   fi
 }
 
 scan amaru_u.htm
 scan bhakirpu.htm
 scan bhall_pu.htm
-./read_gretil.py bharst_u.htm --print_identified_verses=none --print_unidentified_verses=none
+../read_gretil.py bharst_u.htm --print_identified_verses=none --print_unidentified_verses=none
 scan kakumspu.htm
-./read_gretil.py kragh_pu.htm --print_identified_verses=none --print_unidentified_verses=none
+../read_gretil.py kragh_pu.htm --print_identified_verses=none --print_unidentified_verses=none
 scan maghspvu.htm
 scan meghdk_u.htm
 scan msubhs_u.htm
 scan nkalivpu.htm
 scan ramodtpu.htm
 
-python generate_stats_table.py > stats_table.html
+PYTHONPATH=.. python generate_stats_table.py > stats_table.html
