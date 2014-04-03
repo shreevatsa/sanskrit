67fe52882348afbd08da29e32d2edeaf367f9cb5
Shreevatsa R <shreevatsa.public@gmail.com>
2014-04-03 20:02:24 +0530

    cleanup

--- transliterate.py
+++ transliterate.py
@@ -3,23 +3,23 @@
 """Transliteration.
 
 The simplest case of transliteration is when each character of input corresponds
-to a unique character of output. When you see 'e' output <e>, when you see 'y'
+to a unique string of output. When you see 'e' output <e>, when you see 'y'
 output <y>, etc. In this case the code would be rather simple too: set up a
 "replacement" lookup table, and
 
     t = ''.join(replacement[c] for c in s)
 
-The wrinkle, which prevents this approach from working, is cases where we may
-not be able to output until seeing more characters of input. When you see 'a',
-you need to wait and see whether the next character is 'i'; can't output based
-on seeing 'k' without waiting to check whether the next character is 'h'.
+The wrinkle that prevents such an approach from working is that in our case, you
+may not be able to output until seeing multiple characters of input. When you
+see 'a', you need to wait and see whether the next character is 'i'. And on
+seeing 'k', you still need to check whether the next character is 'h'.
 
 This can be solved by ordering and applying the substitution rules in, say,
 descending order of length (so that 'ai' occurs before 'a').
 
 Another solution (which we use) is to create and run a state machine. When you
-see 'e' from the default state, output <e> and return to the default state, but
-when you see 'k', go into a state where
+see 'e' when in the default state, output <e> and return to the default state,
+but when you see 'k', go into a state where
 
 - if the next character is 'h', output 'kh' and go to the default state (having
   consumed both characters)
@@ -62,7 +62,7 @@ def _LongestRead(state_machine, text):
     where = where.get(c)
     if not where:
       return (num_matched, replacement)
-    if '' in where:
+    if '' in where:  # If there's a key that terminates here
       num_matched = num_seen
       replacement = where['']
   return (num_matched, replacement)
@@ -86,6 +86,6 @@ def Transliterate(state_machine, text, ignore=None):
         unparsed_characters.add(char)
       num_parsed += 1
   if unparsed_characters:
-    logging.debug('Could not parse some characters (%s) in %s',
-                  unparsed_characters, text)
+    logging.warning('Could not parse some characters (%s) in %s',
+                    unparsed_characters, text)
   return (transliterated, unparsed_characters)
