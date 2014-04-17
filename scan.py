6277b21efcf9584caac7b5d54cb27a53e87057a1
Shreevatsa R <shreevatsa.public@gmail.com>
2014-04-17 10:14:31 +0530

    clean up further: we don't need the actual syllables

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-"""Given lines in SLP1, converts them to a pattern of laghus and gurus."""
+"""Given lines in SLP1, converts them to patterns of laghus and gurus."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -9,37 +9,9 @@ import re
 import slp1
 
 
-# See ScanVerse: this can be replaced by functions below
-def _PatternFromLine(text):
-  """Given SLP1 text, returns its metrical pattern (string of 'L's and 'G's)."""
-  orig_text = text
-  # A regular-expression "character class" for each type
-  consonant = slp1.CONSONANT
-  short_vowel = slp1.SHORT_VOWEL
-  long_vowel = slp1.LONG_VOWEL
-  # Consonants at beginning of text can be ignored
-  text = re.sub('^' + consonant + '+', '', text)
-  # A long vowel followed by any number of consonants is a guru
-  text = re.sub(long_vowel + consonant + '*', '+', text)
-  # A short vowel followed by multiple (>=2) consonants is a guru
-  text = re.sub(short_vowel + consonant + '{2,}', '+', text)
-  # # TODO(shreevatsa): Should short_vowel + visarga be special-cased to guru?
-  # text = re.sub(short_vowel + 'H$', '+', text)
-  # Any short vowel still left is a laghu; remove it and following consonant
-  text = re.sub(short_vowel + consonant + '?', '-', text)
-  assert re.match('^[+-]*$', text), (orig_text, text)
-  text = text.replace('-', 'L')
-  text = text.replace('+', 'G')
-  assert re.match('^[LG]*$', text)
-  return text
-
-
 def ScanVerse(lines):
   cleaned_lines = _MoveConsonants(lines)
-  old = [_PatternFromLine(line) for line in cleaned_lines]
-  new = [''.join(s[1] for s in _Syllables(line)) for line in cleaned_lines]
-  assert old == new, (old, new, _Syllables(line))
-  return new
+  return [_Scan(line) for line in cleaned_lines]
 
 
 def _MoveConsonants(verse_lines):
@@ -52,27 +24,20 @@ def _MoveConsonants(verse_lines):
 
 def _StripInitialConsonants(text):
   m = re.match(slp1.CONSONANT + '+', text)
-  if m:
-    initial = m.group()
-  else:
-    initial = ''
+  initial = m.group() if m else ''
   return (initial, text[len(initial):])
 
 
 def _Weight(syllable):
-  """Whether a syllable is laghu or guru."""
+  """Whether a syllable (vowel + sequence of consonants) is laghu or guru."""
   assert re.match('^%s%s*$' % (slp1.ANY_VOWEL, slp1.CONSONANT), syllable)
-  if re.search(slp1.LONG_VOWEL, syllable):
-    return 'G'
-  if re.search(slp1.CONSONANT + '{2,}', syllable):
-    return 'G'
+  if re.search(slp1.LONG_VOWEL, syllable): return 'G'
+  if re.search(slp1.CONSONANT + '{2,}', syllable): return 'G'
   return 'L'
 
 
-def _Syllables(text):
-  (initial_consonants, text) = _StripInitialConsonants(text)
+def _Scan(text):
+  (unused_initial_consonants, text) = _StripInitialConsonants(text)
   syllables = re.findall(slp1.ANY_VOWEL + slp1.CONSONANT + '*', text)
-  syllables = [(s, _Weight(s)) for s in syllables]
-  if syllables:
-    syllables[0] = (initial_consonants + syllables[0][0], syllables[0][1])
-  return syllables
+  assert ''.join(syllables) == text
+  return ''.join(_Weight(s) for s in syllables)
