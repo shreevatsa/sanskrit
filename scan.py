60e440abfab7b1ba44256f97ced7ef2043a96d5b
Shreevatsa R <shreevatsa.public@gmail.com>
2014-04-17 10:21:20 +0530

    move out regex

--- scan.py
+++ scan.py
@@ -8,6 +8,8 @@ from __future__ import unicode_literals
 import re
 import slp1
 
+_SYLLABLE_RE = slp1.ANY_VOWEL + slp1.CONSONANT + '*'
+
 
 def ScanVerse(lines):
   cleaned_lines = _MoveConsonants(lines)
@@ -30,14 +32,15 @@ def _StripInitialConsonants(text):
 
 def _Weight(syllable):
   """Whether a syllable (vowel + sequence of consonants) is laghu or guru."""
-  assert re.match('^%s%s*$' % (slp1.ANY_VOWEL, slp1.CONSONANT), syllable)
+  assert re.match('^%s$' % _SYLLABLE_RE, syllable)
   if re.search(slp1.LONG_VOWEL, syllable): return 'G'
   if re.search(slp1.CONSONANT + '{2,}', syllable): return 'G'
   return 'L'
 
 
 def _Scan(text):
+  # Even after _MoveConsonants, leading consonants are present in first line.
   (unused_initial_consonants, text) = _StripInitialConsonants(text)
-  syllables = re.findall(slp1.ANY_VOWEL + slp1.CONSONANT + '*', text)
-  assert ''.join(syllables) == text
+  syllables = re.findall(_SYLLABLE_RE, text)
+  assert text == ''.join(syllables)
   return ''.join(_Weight(s) for s in syllables)
