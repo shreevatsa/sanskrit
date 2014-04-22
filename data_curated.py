8d81b74977d6c36a5791e2b3fd3bcf5be5544b85
Shreevatsa R <shreevatsa.public@gmail.com>
2014-04-22 18:50:22 +0530

    start moving data to separate files

---
+++ data_curated.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""Data for well-known metres."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+curated_vrtta_data = [
+    # Bhartṛhari
+    ('Upajāti', '. G L G G L L G L G .'),
+    # Bhartṛhari
+    ('Rathoddhatā', 'G L G L L L G L G L G'),
+    # Bhāravi
+    ('Svāgatā', 'G L G L L L G L L G G'),
+    # Bhartṛhari
+    ('Drutavilambitam', 'L L L G L L G L L G L G'),
+    # Māgha
+    ('Mañjubhāṣiṇī', 'L L G L G L L L G L G L G'),
+    # Bhartṛhari
+    ('Śālinī', 'G G G G — G L G G L G G'),
+    # Bhāravi
+    ('Praharṣiṇī', 'G G G L L L L G L G L G G'),
+    # Bhāravi
+    ('Pramitākṣarā', 'L L G L G L L L G L L G'),
+    # Bhartṛhari
+    # TODO(shreevatsa): pādānta-lagu whitelist: +Vasantatilakā, -Śālinī
+    ('Vasantatilakā', 'G G L G L L L G L L G L G G'),
+    # Bhartṛhari
+    ('Mālinī', 'L L L L L L G G — G L G G L G G'),
+    # Meghadūta
+    ('Mandākrāntā', 'G G G G — L L L L L G — G L G G L G G'),
+    # Bhartṛhari
+    ('Śikhariṇī', 'L G G G G G – L L L L L G G — L L L G'),
+    # Bhartṛhari
+    ('Hariṇī', 'L L L L L G — G G G G — L G L L G L G'),
+    # Bhartṛhari
+    ('Pṛthvī', 'L G L L L G L G—L L L G L G G L G'),
+    # Bhartṛhari
+    ('Śārdūlavikrīḍitam',
+     'G G G L L G L G L L L G — G G L G G L G'),
+    # Bhartṛhari
+    ('Sragdharā',
+     'G G G G L G G — L L L L L L G — G L G G L G G'),
+    # Bhartṛhari
+    ('Viyoginī', ['L L G   L L G L G L G', 'L L G G L L G L G L G']),
+    # Bhāravi
+    ('Aupacchandasikam (Vasantamālikā) (Upodgatā)',
+     ['L L G   L L G L G L G G', 'L L G G L L G L G L G G']),
+    # Bhāravi
+    ('Aparavaktrā', ['L L L L L L G — L G L G', 'L L L L G — L L G L G L G']),
+    # Bhartṛhari
+    ('Puṣpitāgrā', ['L L L L L L G L G L G G', 'L L L L G L L G L G L G G']),
+    # Bhāravi
+    ('Udgatā', ['L L  G  L  G  L L L G L',
+                'L L L L L  G  L  G  L G',
+                ' G  L L L L L L  G  L L G',
+                'L L  G  L  G  L L L  G  L G L G']),
+    # Bhartṛhari
+    ('Dodhakam', 'G L L G L L G L L G G'),
+    # Bhāravi
+    ('Matta-mayūram', 'G G G G – G L L G G – L L G G'),
+    # Bhāravi
+    ('Kṣamā (Candrikā, Utpalinī)', 'LLLLLLGGLGGLG'),
+    # Bhāravi
+    ('Prabhā (Mandākinī)', 'LLLLLLGLGGLG'),
+    # Bhāravi
+    ('Jaladharamālā', 'GGGGLLLLGGGG'),
+    # Bhāravi
+    ('Jaloddhatagatiḥ', 'LGLLLGLGLLLG'),
+    # Bhāravi
+    ('Madhyakṣāmā (Haṃsaśyenī, Kuṭila, Cūḍāpīḍam)',
+     'G G G G L L L L L L G G G G'),
+    # Bhāravi
+    ('Vaṃśapatrapatitam (Vaṃśadala)',
+     'G L L G L G L L L G L L L L L L G'),
+    # Māgha
+    ('Rucirā (Prabhāvatī)', 'L G L G L L L L G L G L G'),
+    # Raghuvamśa (hard to believe, but there it is)
+    ('Nārācam', 'L L L L L L G L G G L G G L G G L G'),
+
+    ('Bhujañgaprayātam', 'L G G L G G L G G L G G'),
+    ('Toṭakam', 'L L G L L G L L G L L G'),
+    ('Sragviṇī', 'G L G G L G G L G G L G'),
+    ('Cārucāmaram', 'G L G L G L G L G L G L G L G'),
+    ('Pañcacāmaram', 'L G L G L G L G L G L G L G L G'),
+    ('Kokilalam (Nardaṭakam)', 'L L L L G L G L L L G — L L G L L G'),
+    ('Mallikāmālā (Matta-kokilā)', 'G L G L L G L G L L G L G L L G L G'),
+    ('Aśvadhāṭī (Sitastavaka?)',
+     'G G L G L L L – G G L G L L L – G G L G L L L G'),
+    ('Śivatāṇḍava',
+     'L G L L  L G L L  L G L L  L G L L  L G L L  L G L L  L G'),
+    # # AddMatravrtta('Pādākulakam (and many other names)', ['4 * 4'] * 4)
+    ('Mālatī', 'L L L L G L L G L G L G'),
+    ('Madīrā', 'G L L  G L L  G L L  G L L  G L L  G L L  G L L  G'),
+    ('Vidyunmālā', 'G G G G G G G G'),
+    ]
--- metrical_data.py
+++ metrical_data.py
@@ -9,8 +9,9 @@ from __future__ import unicode_literals
 import itertools
 import re
 
-import match_result
+import data_curated
 
+import match_result
 
 known_metre_regexes = []
 known_metre_patterns = {}
@@ -380,97 +381,6 @@ def _AddKarambajati():
   #                'L G L G G L L G L G L G'])
 
 
-_curated_vrtta_data = [
-    # Bhartṛhari
-    ('Upajāti', '. G L G G L L G L G .'),
-    # Bhartṛhari
-    ('Rathoddhatā', 'G L G L L L G L G L G'),
-    # Bhāravi
-    ('Svāgatā', 'G L G L L L G L L G G'),
-    # Bhartṛhari
-    ('Drutavilambitam', 'L L L G L L G L L G L G'),
-    # Māgha
-    ('Mañjubhāṣiṇī', 'L L G L G L L L G L G L G'),
-    # Bhartṛhari
-    ('Śālinī', 'G G G G — G L G G L G G'),
-    # Bhāravi
-    ('Praharṣiṇī', 'G G G L L L L G L G L G G'),
-    # Bhāravi
-    ('Pramitākṣarā', 'L L G L G L L L G L L G'),
-    # Bhartṛhari
-    # TODO(shreevatsa): pādānta-lagu whitelist: +Vasantatilakā, -Śālinī
-    ('Vasantatilakā', 'G G L G L L L G L L G L G G'),
-    # Bhartṛhari
-    ('Mālinī', 'L L L L L L G G — G L G G L G G'),
-    # Meghadūta
-    ('Mandākrāntā', 'G G G G — L L L L L G — G L G G L G G'),
-    # Bhartṛhari
-    ('Śikhariṇī', 'L G G G G G – L L L L L G G — L L L G'),
-    # Bhartṛhari
-    ('Hariṇī', 'L L L L L G — G G G G — L G L L G L G'),
-    # Bhartṛhari
-    ('Pṛthvī', 'L G L L L G L G—L L L G L G G L G'),
-    # Bhartṛhari
-    ('Śārdūlavikrīḍitam',
-     'G G G L L G L G L L L G — G G L G G L G'),
-    # Bhartṛhari
-    ('Sragdharā',
-     'G G G G L G G — L L L L L L G — G L G G L G G'),
-    # Bhartṛhari
-    ('Viyoginī', ['L L G   L L G L G L G', 'L L G G L L G L G L G']),
-    # Bhāravi
-    ('Aupacchandasikam (Vasantamālikā) (Upodgatā)',
-     ['L L G   L L G L G L G G', 'L L G G L L G L G L G G']),
-    # Bhāravi
-    ('Aparavaktrā', ['L L L L L L G — L G L G', 'L L L L G — L L G L G L G']),
-    # Bhartṛhari
-    ('Puṣpitāgrā', ['L L L L L L G L G L G G', 'L L L L G L L G L G L G G']),
-    # Bhāravi
-    ('Udgatā', ['L L  G  L  G  L L L G L',
-                'L L L L L  G  L  G  L G',
-                ' G  L L L L L L  G  L L G',
-                'L L  G  L  G  L L L  G  L G L G']),
-    # Bhartṛhari
-    ('Dodhakam', 'G L L G L L G L L G G'),
-    # Bhāravi
-    ('Matta-mayūram', 'G G G G – G L L G G – L L G G'),
-    # Bhāravi
-    ('Kṣamā (Candrikā, Utpalinī)', 'LLLLLLGGLGGLG'),
-    # Bhāravi
-    ('Prabhā (Mandākinī)', 'LLLLLLGLGGLG'),
-    # Bhāravi
-    ('Jaladharamālā', 'GGGGLLLLGGGG'),
-    # Bhāravi
-    ('Jaloddhatagatiḥ', 'LGLLLGLGLLLG'),
-    # Bhāravi
-    ('Madhyakṣāmā (Haṃsaśyenī, Kuṭila, Cūḍāpīḍam)',
-     'G G G G L L L L L L G G G G'),
-    # Bhāravi
-    ('Vaṃśapatrapatitam (Vaṃśadala)',
-     'G L L G L G L L L G L L L L L L G'),
-    # Māgha
-    ('Rucirā (Prabhāvatī)', 'L G L G L L L L G L G L G'),
-    # Raghuvamśa (hard to believe, but there it is)
-    ('Nārācam', 'L L L L L L G L G G L G G L G G L G'),
-
-    ('Bhujañgaprayātam', 'L G G L G G L G G L G G'),
-    ('Toṭakam', 'L L G L L G L L G L L G'),
-    ('Sragviṇī', 'G L G G L G G L G G L G'),
-    ('Cārucāmaram', 'G L G L G L G L G L G L G L G'),
-    ('Pañcacāmaram', 'L G L G L G L G L G L G L G L G'),
-    ('Kokilalam (Nardaṭakam)', 'L L L L G L G L L L G — L L G L L G'),
-    ('Mallikāmālā (Matta-kokilā)', 'G L G L L G L G L L G L G L L G L G'),
-    ('Aśvadhāṭī (Sitastavaka?)',
-     'G G L G L L L – G G L G L L L – G G L G L L L G'),
-    ('Śivatāṇḍava',
-     'L G L L  L G L L  L G L L  L G L L  L G L L  L G L L  L G'),
-    # # AddMatravrtta('Pādākulakam (and many other names)', ['4 * 4'] * 4)
-    ('Mālatī', 'L L L L G L L G L G L G'),
-    ('Madīrā', 'G L L  G L L  G L L  G L L  G L L  G L L  G L L  G'),
-    ('Vidyunmālā', 'G G G G G G G G'),
-    ]
-
-
 def InitializeData():
   """Add all known metres to the data structures."""
   _AddAnustup()
@@ -483,7 +393,7 @@ def InitializeData():
 
   _AddKarambajati()
 
-  for (name, description) in _curated_vrtta_data:
+  for (name, description) in data_curated.curated_vrtta_data:
     samatva = None
     need_regex = None
     if isinstance(description, list):
