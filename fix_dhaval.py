2daee9c2f4841fb91d150a0f445552651ca5ab18
Shreevatsa R <shreevatsa.public@gmail.com>
2014-05-06 17:05:35 +0530

    fix long lines

--- fix_dhaval.py
+++ fix_dhaval.py
@@ -13,13 +13,16 @@ for line in lines:
   if re.match(prefix1, line):
     m = re.match(prefix1 + r"\('(?P<name>.*)', '(?P<line>.*)'\)", line)
     assert m
-    name = m.group('name'); assert name == name.strip()
+    name = m.group('name')
+    assert name == name.strip()
     each = m.group('line').strip()
     line = "('%s', '%s')," % (name, each)
   elif re.match(prefix2, line):
-    m = re.match(prefix2 + r"\('(?P<name>.*)', '(?P<first>.*)', '(?P<second>.*)'\)", line)
+    m = re.match(prefix2 +
+                 r"\('(?P<name>.*)', '(?P<first>.*)', '(?P<second>.*)'\)", line)
     assert m
-    name = m.group('name'); assert name == name.strip()
+    name = m.group('name')
+    assert name == name.strip()
     odd = m.group('first').strip()
     even = m.group('second').strip()
     line = "('%s', ['%s', '%s'])," % (name, odd, even)
@@ -27,6 +30,10 @@ for line in lines:
     assert re.match(prefix3, line)
     m = re.match(prefix3 + r"\('(?P<name>.*)', '(?P<line1>.*)', '(?P<line2>.*)', '(?P<line3>.*)', '(?P<line4>.*)'\)", line)
     assert m
-    name = m.group('name'); assert name == name.strip()
-    line = "('%s', ['%s', '%s', '%s', '%s'])," % (name, m.group('line1'), m.group('line2'), m.group('line3'), m.group('line4'))
+    name = m.group('name')
+    assert name == name.strip()
+    line = "('%s', ['%s', '%s', '%s', '%s'])," % (name, m.group('line1'),
+                                                  m.group('line2'),
+                                                  m.group('line3'),
+                                                  m.group('line4'))
   print line
