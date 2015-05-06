b9c03aee1f5373336b03340c12611f6eda367eb7
Shreevatsa R <shreevatsa.public@gmail.com>
2015-05-06 10:55:38 -0700

    Fixes #73: rehauls scanning to deal with pesky last-syllable-of-last-line-ends-with-consonant problem.

--- scan.py
+++ scan.py
@@ -13,14 +13,16 @@ _SYLLABLE_RE = slp1.VOWEL_RE + slp1.CONSONANT_RE + '*'
 
 def ScanVerse(lines):
   cleaned_lines = _MoveInitialConsonants(lines)
-  return [_Scan(line) for line in cleaned_lines]
+  return [_ScanVowelInitialLine(cleaned_lines[i], i == len(cleaned_lines) - 1)
+          for i in range(len(cleaned_lines))]
 
 
 def _MoveInitialConsonants(verse_lines):
   """Move initial consonants of each line to the end of the previous line."""
-  for i in xrange(1, len(verse_lines)):
+  for i in xrange(len(verse_lines)):
     (consonants, verse_lines[i]) = _StripInitialConsonants(verse_lines[i])
-    verse_lines[i - 1] += consonants
+    if i > 0 and verse_lines[i - 1]:
+      verse_lines[i - 1] += consonants
   return verse_lines
 
 
@@ -30,17 +32,19 @@ def _StripInitialConsonants(text):
   return (initial, text[len(initial):])
 
 
-def _Weight(syllable):
+def _Weight(syllable, is_final_syllable_of_line=False):
   """Whether a syllable (vowel + sequence of consonants) is laghu or guru."""
-  assert re.match('^%s$' % _SYLLABLE_RE, syllable)
+  assert re.match('^%s$' % _SYLLABLE_RE, syllable), syllable
   if re.search(slp1.LONG_VOWEL_RE, syllable): return 'G'
   if re.search(slp1.CONSONANT_RE + '{2,}', syllable): return 'G'
+  if is_final_syllable_of_line and re.search(slp1.CONSONANT_RE, syllable):
+    return 'G'
   return 'L'
 
 
-def _Scan(text):
-  # Even after moving consonants, leading consonants are present in first line.
-  (_, text) = _StripInitialConsonants(text)
+def _ScanVowelInitialLine(text, is_last_line=False):
   syllables = re.findall(_SYLLABLE_RE, text)
-  assert text == ''.join(syllables)
-  return ''.join(_Weight(s) for s in syllables)
+  assert text == ''.join(syllables), text
+  if len(syllables) == 0: return ''
+  return (''.join(_Weight(s) for s in syllables[:-1]) +
+          _Weight(syllables[-1], is_final_syllable_of_line=is_last_line))
