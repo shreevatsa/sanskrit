e0c50935772421661262a1127d3a49b0a8bd7674
Shreevatsa R <shreevatsa.public@gmail.com>
2015-05-07 13:30:39 -0700

    inkscape seems to be wonky on my system

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 # Snakefood
-files=$(find . -iname '*.py' ! -path '*_test.py' ! -path './print_utils.py' ! -path './gretil_stats/generate_stats_table.py')
+files=$(find . -iname '*.py' ! -path '*_test.py' ! -path './print_utils.py' ! -path './gretil_stats/generate_stats_table.py' ! -path './poor_enums.py')
 sfood --internal --internal ${files} --ignore-unused | sfood-graph | dot -Tsvg > deps.svg
-inkscape -z -e deps.png deps.svg
-rm deps.svg
+# inkscape -z -e deps.png deps.svg
+# rm deps.svg
