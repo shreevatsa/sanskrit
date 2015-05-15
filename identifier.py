705a6a8b5ffeb852b7f85203729861536d10bdc8
Shreevatsa R <shreevatsa.public@gmail.com>
2015-05-15 10:47:59 -0700

    change of plans -- just commit the current state

--- identifier.py
+++ identifier.py
@@ -35,103 +35,109 @@ class Identifier(object):
     self.halves_info = []
     self.quarters_info = []
 
-  def IdentifyFromPatternLines(self, pattern_lines):
-    """Main function. Takes patterns of verse lines and identifies metres."""
-    self._Reset()
+  def Identify(self, pattern_lines, input_type):
+    parts = _Parts(pattern_lines)
+    ret = set()
+    for (part, match_with) in _Strategies(input_type):
+      # e.g. strategies contains ('full', ('known_metre_patterns', 'known_metre_regexes'))
+      ret = ret.union(self._Lookup(parts.get(part), match_with))
 
-    # Too many lines => probably multiple verses.
-    if len(pattern_lines) > 12:
-      self.global_info.append('Error: too many lines in verse. '
-                              'Perhaps these are multiple verses?')
-      return (False, [])
+  # def IdentifyFromPatternLines(self, pattern_lines):
+  #   """Main function. Takes patterns of verse lines and identifies metres."""
+  #   self._Reset()
 
-    full_verse = ''.join(pattern_lines)
-    assert _IsPattern(full_verse)
-    (global_results, self.global_info) = self._MetresAndInfo(full_verse,
-                                                             try_partial=False)
-    self.global_info = [self.global_info]
+  #   # Too many lines => probably multiple verses.
+  #   if len(pattern_lines) > 12:
+  #     self.global_info.append('Error: too many lines in verse. '
+  #                             'Perhaps these are multiple verses?')
+  #     return (False, [])
 
-    lines_results = []
-    for (i, line) in enumerate(pattern_lines):
-      (results, info) = self._MetresAndInfo(line)
-      self.lines_info.append('  Line %d: %s' % (i + 1, info))
-      lines_results = _MergeResults([lines_results, results])
+  #   full_verse = ''.join(pattern_lines)
+  #   if not _IsPattern(full_verse): return (False, [])
+  #   (global_results, self.global_info) = self._MetresAndInfo(full_verse, try_partial=False)
+  #   self.global_info = [self.global_info]
 
-    if global_results:
-      return (True, _MergeResults([global_results]))
+  #   lines_results = []
+  #   for (i, line) in enumerate(pattern_lines):
+  #     (results, info) = self._MetresAndInfo(line)
+  #     self.lines_info.append('  Line %d: %s' % (i + 1, info))
+  #     lines_results = _MergeResults([lines_results, results])
 
-    halves_results = []
-    quarters_results = []
-    for (ab, cd) in _SplitHalves(full_verse):
-      (ab_results, ab_info) = self._MetresAndInfo(ab)
-      (cd_results, cd_info) = self._MetresAndInfo(cd)
-      self.halves_info.append('  Half 1: %s' % ab_info)
-      self.halves_info.append('  Half 2: %s' % cd_info)
-      halves_results = _MergeResults([halves_results, ab_results, cd_results])
-    for quarters in _SplitQuarters(full_verse):
-      all_results = []
-      for (i, quarter_i) in enumerate(quarters):
-        (results, info) = self._MetresAndInfo(quarter_i)
-        self.quarters_info.append('  Quarter %d: %s' % (i + 1, info))
-        all_results.extend(results)
-      quarters_results = _MergeResults([quarters_results, all_results])
-    return (False, _MergeResults([global_results, lines_results,
-                                  halves_results, quarters_results]))
+  #   if global_results:
+  #     return (True, _MergeResults([global_results]))
 
-  def _MetresAndInfo(self, pattern, try_partial=True):
-    assert _IsPattern(pattern)
-    results = self._IdentifyPattern(pattern, try_partial)
-    name = match_result.Names(results) if results else 'unknown'
-    info = ('pattern %s (%d syllables, %d mātras) is %s' %
-            (pattern, len(pattern), _MatraCount(pattern), name))
-    return (results, info)
+  #   halves_results = []
+  #   quarters_results = []
+  #   for (ab, cd) in _SplitHalves(full_verse):
+  #     (ab_results, ab_info) = self._MetresAndInfo(ab)
+  #     (cd_results, cd_info) = self._MetresAndInfo(cd)
+  #     self.halves_info.append('  Half 1: %s' % ab_info)
+  #     self.halves_info.append('  Half 2: %s' % cd_info)
+  #     halves_results = _MergeResults([halves_results, ab_results, cd_results])
+  #   for quarters in _SplitQuarters(full_verse):
+  #     all_results = []
+  #     for (i, quarter_i) in enumerate(quarters):
+  #       (results, info) = self._MetresAndInfo(quarter_i)
+  #       self.quarters_info.append('  Quarter %d: %s' % (i + 1, info))
+  #       all_results.extend(results)
+  #     quarters_results = _MergeResults([quarters_results, all_results])
+  #   return (False, _MergeResults([global_results, lines_results,
+  #                                 halves_results, quarters_results]))
 
-  def _IdentifyPattern(self, pattern, try_partial=True):
-    """Given metrical pattern (string of L's and G's), identifies metre."""
-    full_results = _IdentifyPatternFrom(pattern, self.metrical_data.known_metre_patterns, self.metrical_data.known_metre_regexes)
-    partial_results = []
-    if try_partial:
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_half_patterns, self.metrical_data.known_half_regexes))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_first_half_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_second_half_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_odd_pada_regexes))
-      partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_even_pada_regexes))
-      partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_pada_regexes))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_odd_pada_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_even_pada_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_1_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_2_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_3_patterns, []))
-      partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_4_patterns, []))
-    ret = full_results + partial_results
-    return ret
+  # def _MetresAndInfo(self, pattern, try_partial=True):
+  #   assert _IsPattern(pattern)
+  #   results = self._IdentifyPattern(pattern, try_partial)
+  #   name = match_result.Names(results) if results else 'unknown'
+  #   info = ('pattern %s (%d syllables, %d mātras) is %s' %
+  #           (pattern, len(pattern), _MatraCount(pattern), name))
+  #   return (results, info)
 
-def _IdentifyPatternFrom(pattern, known_patterns, known_regexes):
-  """Identify pattern from given data."""
-  results = known_patterns.get(pattern)
-  if results:
-    return results
-  # Stop at the first match so that e.g. 'Āryā (strict schema)' hides 'Āryā (loose schema)'
-  for (regex, metre_name) in known_regexes:
-    if regex.match(pattern):
-      return [metre_name]
-  return []
+  # def _IdentifyPattern(self, pattern, try_partial=True):
+  #   """Given metrical pattern (string of L's and G's), identifies metre."""
+  #   full_results = _IdentifyPatternFrom(pattern, self.metrical_data.known_metre_patterns, self.metrical_data.known_metre_regexes)
+  #   partial_results = []
+  #   if try_partial:
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_half_patterns, self.metrical_data.known_half_regexes))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_first_half_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_second_half_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_odd_pada_regexes))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_even_pada_regexes))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, {}, self.metrical_data.known_pada_regexes))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_odd_pada_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_even_pada_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_1_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_2_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_3_patterns, []))
+  #     partial_results.extend(_IdentifyPatternFrom(pattern, self.metrical_data.known_pada_4_patterns, []))
+  #   ret = full_results + partial_results
+  #   return ret
+
+# def _IdentifyPatternFrom(pattern, known_patterns, known_regexes):
+#   """Identify pattern from given data."""
+#   results = known_patterns.get(pattern)
+#   if results:
+#     return results
+#   # Stop at the first match so that e.g. 'Āryā (strict schema)' hides 'Āryā (loose schema)'
+#   for (regex, metre_name) in known_regexes:
+#     if regex.match(pattern):
+#       return [metre_name]
+#   return []
 
 
-def _MatraCount(pattern):
-  return sum(2 if c == 'G' else 1 for c in pattern)
+# def _MatraCount(pattern):
+#   return sum(2 if c == 'G' else 1 for c in pattern)
 
 
-def _MergeResults(results_lists):
-  """Returns a list of metre names without duplicates."""
-  def _UniqList(expr):
-    return list(collections.OrderedDict.fromkeys(expr))
-  names = []
-  for results_list in results_lists:
-    for result in results_list:
-      names.append(result)
-  return _UniqList(names)
+# def _MergeResults(results_lists):
+#   """Returns a list of metre names without duplicates."""
+#   def _UniqList(expr):
+#     return list(collections.OrderedDict.fromkeys(expr))
+#   names = []
+#   for results_list in results_lists:
+#     for result in results_list:
+#       names.append(result)
+#   return _UniqList(names)
 
 
 def _SplitHalves(full_pattern):
@@ -206,4 +212,130 @@ def _SplitQuarters(full_pattern):
 
 
 def _IsPattern(pattern):
-  return re.match('^[LG]*$', pattern)
+  return re.match('^[LG]+$', pattern)
+
+
+# TODO(shreevatsa): Distinguish between exact (unique) and approximate halves/padas.
+def _Parts(pattern_lines):
+  """ {
+    'full': [...],
+    'half_1': [...],
+    'half_2': [...],
+    'pada_1': [...],
+    'pada_2': [...],
+    'pada_3': [...],
+    'pada_4': [...],
+    'lines': [...] (can overlap with pada_n / half_n)
+  }
+  """
+  pattern_lines = [line for line in pattern_lines if _IsPattern(line)]
+  full_pattern = ''.join(pattern_lines)
+  ret = {}
+  def add(x, e): ret.setdefault(x, set()).add(e)
+  ret['full'] = [full_pattern]
+  for (ab, cd) in _SplitHalves(full_pattern):
+    add('half_1', ab)
+    add('half_2', cd)
+  for (a, b, c, d) in _SplitQuarters(full_pattern):
+    add('pada_1', a)
+    add('pada_2', b)
+    add('pada_3', c)
+    add('pada_4', d)
+  ret['lines'] = pattern_lines
+  n = len(pattern_lines)
+  if n % 2 == 0:
+    half_1 = ''.join(pattern_lines[ : n/2])
+    add('half_1', half_1)
+    for (a, b) in _SplitHalves(half_1):
+      add('pada_1', a)
+      add('pada_2', b)
+    half_2 = ''.join(pattern_lines[n/2 : ])
+    add('half_2', half_2)
+    for (c, d) in _SplitHalves(half_2):
+      add('pada_3', c)
+      add('pada_4', d)
+  if n % 4 == 0:
+    add('pada_1', ''.join(pattern_lines[: n/4]))
+    add('pada_2', ''.join(pattern_lines[n/4 : n/2]))
+    add('pada_3', ''.join(pattern_lines[n/2 : 3*n/4]))
+    add('pada_4', ''.join(pattern_lines[3*n/4 : ]))
+
+def _Strategies(input_type):
+  left = ['full',
+          'half_1',
+          'half_2',
+          'pada_1',
+          'pada_2',
+          'pada_3',
+          'pada_4',
+          'lines']
+  right = ['known_metre_patterns',
+           'known_metre_regexes',
+           'known_half_patterns',
+           'known_half_regexes',
+           'known_first_half_patterns',
+           'known_second_half_patterns',
+           'known_pada_patterns',
+           'known_pada_regexes',
+           'known_odd_pada_patterns',
+           'known_odd_pada_regexes',
+           'known_even_pada_patterns',
+           'known_even_pada_regexes',
+           'known_pada_1_patterns',
+           'known_pada_2_patterns',
+           'known_pada_3_patterns',
+           'known_pada_4_patterns']
+  if input_type == 'full':
+    return [('full', 'known_metre_patterns'),
+            ('full', 'known_metre_regexes'),
+
+            ('half_1', 'known_half_patterns'),
+            ('half_1', 'known_half_regexes'),
+            ('half_1', 'known_first_half_patterns'),
+
+            ('half_2', 'known_half_patterns'),
+            ('half_2', 'known_half_regexes'),
+            ('half_2', 'known_second_half_patterns'),
+
+            ('pada_1', 'known_pada_patterns'),
+            ('pada_1', 'known_pada_regexes'),
+            ('pada_1', 'known_odd_pada_patterns'),
+            ('pada_1', 'known_odd_pada_regexes'),
+            ('pada_1', 'known_pada_1_patterns'),
+
+            ('pada_2', 'known_pada_patterns'),
+            ('pada_2', 'known_pada_regexes'),
+            ('pada_2', 'known_even_pada_patterns'),
+            ('pada_2', 'known_even_pada_regexes'),
+            ('pada_2', 'known_pada_2_patterns'),
+
+            ('pada_3', 'known_pada_patterns'),
+            ('pada_3', 'known_pada_regexes'),
+            ('pada_3', 'known_odd_pada_patterns'),
+            ('pada_3', 'known_odd_pada_regexes'),
+            ('pada_3', 'known_pada_3_patterns'),
+
+            ('pada_4', 'known_pada_patterns'),
+            ('pada_4', 'known_pada_regexes'),
+            ('pada_4', 'known_even_pada_patterns'),
+            ('pada_4', 'known_even_pada_regexes'),
+            ('pada_4', 'known_pada_4_patterns')]
+  if input_type == 'half':
+    return [('full', 'known_half_patterns'),
+            ('full', 'known_half_regexes'),
+            ('full', 'known_first_half_patterns'),
+            ('full', 'known_second_half_patterns'),
+
+            ('half_1', 'known_pada_patterns'),
+            ('half_1', 'known_pada_regexes'),
+            ('half_1', 'known_odd_pada_patterns'),
+            ('half_1', 'known_odd_pada_regexes'),
+            ('half_1', 'known_pada_1_patterns'),
+            ('half_1', 'known_pada_3_patterns'),
+
+            ('half_2', 'known_pada_patterns'),
+            ('half_2', 'known_pada_regexes'),
+            ('half_2', 'known_even_pada_patterns'),
+            ('half_2', 'known_even_pada_regexes'),
+            ('half_2', 'known_pada_2_patterns'),
+            ('half_2', 'known_pada_4_patterns')]
