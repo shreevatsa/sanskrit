4456cf7002e52177ca745419353dea15b690abe2
Shreevatsa R <shreevatsa.public@gmail.com>
2015-05-18 08:38:47 -0700

    Hack print_utils to print regexes.

--- print_utils.py
+++ print_utils.py
@@ -5,20 +5,20 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
 def ToUnicode(x):
   """Convert x to unicode, whatever it is."""
-  if isinstance(x, unicode):
-    return x
-  if isinstance(x, int):
-    return unicode(x)
-  if isinstance(x, list):
-    return _ListToUnicode(x)
-  if isinstance(x, dict):
-    return _DictToUnicode(x)
+  if isinstance(x, unicode): return x
+  if isinstance(x, int): return unicode(x)
+  if isinstance(x, list): return '[%s]' % ', '.join(ToUnicode(v) for v in x)
+  if isinstance(x, tuple): return '(%s)' % ', '.join(ToUnicode(i) for i in x)
+  if isinstance(x, set): return '{%s}' % ', '.join(sorted(ToUnicode(v) for v in x))
+  if isinstance(x, dict): return _DictToUnicode(x)
+  # A hack for printing regexes
+  if hasattr(x, 'match') and hasattr(x, 'pattern'):
+    assert isinstance(x.pattern, unicode)
+    return 're(%s)' % x.pattern
   # For MatchResult, use x.Name()
-  if callable(getattr(x, 'Name', None)):
-    return x.Name()
+  if callable(getattr(x, 'Name', None)): return x.Name()
   assert False, (x, type(x))
 
 
@@ -32,7 +32,7 @@ def _ListToUnicode(li):
 def _DictToUnicode(d):
   assert isinstance(d, dict)
   ret = '{'
-  for (key, value) in sorted(d.items(), key=lambda x: x[1], reverse=True):
+  for (key, value) in sorted(d.items()):
     ret += '\n  %s: %s' % (ToUnicode(key), ToUnicode(value))
   ret += '\n}'
   assert isinstance(ret, unicode)
