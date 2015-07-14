a21256b80acd47f7c7006ca7640353a245918eb8
Shreevatsa R <shreevatsa.public@gmail.com>
2015-07-13 18:59:51 -0700

    more refactor

--- handle_input.py
+++ handle_input.py
@@ -25,32 +25,21 @@ def _transliterate_and_clean(orig_text):
   return (text, clean_text)
 
 
-def clean_text(text):
-  """Runs some cleaning functions on the text (strip junk, etc.)"""
-  text = read.filters.normalize_nfkc(text)
-  text = read.filters.remove_control_characters(text)
-  text = read.filters.process_html(text)
-  text = text.strip()
-  lines = text.splitlines()
-  (display_lines, cleaned_lines) = _clean_lines(lines)
-
-  debug_output = ['Input read as:']
-  for (number, line) in enumerate(display_lines):
-    transliterated = transliterate.TransliterateForOutput(line)
-    debug_output.append('Line %d: %s' % (number + 1, transliterated))
-  debug_output.append('')
-  logging.debug('\n'.join(debug_output))
-
-  return (display_lines, cleaned_lines)
-
-def _clean_lines(lines):
-  """Clean up the input lines (strip junk, transliterate, break verses)."""
-  cleaned_lines = []
-  display_lines = []
+def _preprocess_and_transliterate(lines):
+  cleaned_and_display_lines = []
   for line in lines:
     line = line.strip()
     (line, n) = read.filters.remove_verse_number(line)
     (line, clean_line) = _transliterate_and_clean(line)
+    cleaned_and_display_lines.append((clean_line, line, n))
+  return cleaned_and_display_lines
+
+
+def _process_breaks(cleaned_and_display_lines):
+  """What to do with blank lines or lines that contained verse numbers."""
+  cleaned_lines = []
+  display_lines = []
+  for (clean_line, line, n) in cleaned_and_display_lines:
     if not clean_line:
       cleaned_lines.append('')
       display_lines.append('')
@@ -64,4 +53,23 @@ def _clean_lines(lines):
   while cleaned_lines and not cleaned_lines[-1]:
     cleaned_lines = cleaned_lines[:-1]
     display_lines = display_lines[:-1]
+  return (display_lines, cleaned_lines)
+
+
+def clean_text(text):
+  """The transliterated text from arbitrary input."""
+  text = read.filters.normalize_nfkc(text)
+  text = read.filters.remove_control_characters(text)
+  text = read.filters.process_html(text)
+  text = text.strip()
+  lines = text.splitlines()
+  (display_lines, cleaned_lines) = _process_breaks(_preprocess_and_transliterate(lines))
+
+  debug_output = ['Input read as:']
+  for (number, line) in enumerate(display_lines):
+    transliterated = transliterate.TransliterateForOutput(line)
+    debug_output.append('Line %d: %s' % (number + 1, transliterated))
+  debug_output.append('')
+  logging.debug('\n'.join(debug_output))
+
   return (display_lines, cleaned_lines)
